353c8c137136addab5e958955c2dbf1f33c4273e
andreaweb <deiainluv@gmail.com>
2018-09-01 11:56:23 -0300

    replacing N/A with more useful information

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -19,18 +19,39 @@ class MovieDetails extends React.Component {
 					<h2 className="movie-title">{this.props.movieDetails.movieDetails.Title}</h2>
 					<span> {this.props.movieDetails.movieDetails.Year }
 						<span className="separator">|</span> 
-						{this.props.movieDetails.movieDetails.Runtime}
+						{	
+							this.props.movieDetails.movieDetails.Runtime == "N/A"
+							? "No Runtime available"
+							: this.props.movieDetails.movieDetails.Runtime
+						} 
 						<span className="separator">|</span> 
-						{this.props.movieDetails.movieDetails.Genre} 
+						{	
+							this.props.movieDetails.movieDetails.Genre == "N/A"
+							? "No genre available"
+							: this.props.movieDetails.movieDetails.Genre
+						} 
 					</span>
 
 					<section className="rating">
-						<Rating rating={this.props.movieDetails.movieDetails.imdbRating} />
-						<span className="number-rating"> {this.props.movieDetails.movieDetails.imdbRating} / 10</span>
+						{ this.props.movieDetails.movieDetails.imdbRating && this.props.movieDetails.movieDetails.imdbRating != "N/A"
+							? <Rating rating={this.props.movieDetails.movieDetails.imdbRating} />
+							: null
+						}
+						
+						<span className="number-rating"> {
+							this.props.movieDetails.movieDetails.imdbRating == "N/A"
+							? " - "
+							: this.props.movieDetails.movieDetails.imdbRating
+						} / 10</span>
 					</section>
 
 					<h5 className="synopsis-title">Sinopse</h5>
-					<p className="movie-synopsis">{this.props.movieDetails.movieDetails.Plot}</p>
+					<p className="movie-synopsis">
+						{this.props.movieDetails.movieDetails.Plot == "N/A" 
+							? "No plot available"
+							: this.props.movieDetails.movieDetails.Plot
+						}
+					</p>
 					
 					<Link to="/">
 						<button className="close-button--mobile">Voltar</button>
