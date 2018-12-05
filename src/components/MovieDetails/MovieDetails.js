7abf6f7e02605158accf5e49ca6fa531839efa95
andreaweb <deiainluv@gmail.com>
2018-12-05 17:05:30 -0200

    improving movie details further

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -7,91 +7,81 @@ import './MovieDetails.css';
 
 class MovieDetails extends React.Component {
 	componentDidMount(){
-		if(this.props.activeMovie.movieDetails.length > 1){
-			console.log('inherited from main')
-		}else{
-			let movieID = this.props.match.params.id;
-			this.props.dispatch(fetchMovieDetails(movieID));
-		}
+		let movieID = this.props.match.params.id;
+		this.props.dispatch(fetchMovieDetails(movieID));
 	}
 	render() {
 		return  (
-			<div>
-			{	this.props.activeMovie.movieDetails.Title ?
-				<div className="movie-details">
-					<div 
-						className="movie-poster" 
-						style={{ backgroundImage: `url(
-							${this.props.activeMovie.movieDetails.Poster !== "N/A" 
-							? this.props.activeMovie.movieDetails.Poster 
-							: null})`
-							}}
-					/>
+			<div className="movie-details">
+				<div 
+					className="movie-poster" 
+					style={{ backgroundImage: `url(
+						${this.props.activeMovie.movieDetails.Poster !== "N/A" 
+						? this.props.activeMovie.movieDetails.Poster 
+						: null})`
+						}}
+				/>
 
-					<main className="movie-info">
-						<h2 className="movie-title">
-							{this.props.activeMovie.movieDetails.Title}
-						</h2>
-						<span> 
-							{this.props.activeMovie.movieDetails.Year }
-							<span className="separator">|</span> 
-							{	
-								this.props.activeMovie.movieDetails.Runtime === "N/A"
-								? "No Runtime available"
-								: this.props.activeMovie.movieDetails.Runtime
-							} 
-							<span className="separator">|</span> 
-							{	
-								this.props.activeMovie.movieDetails.Genre === "N/A"
-								? "No genre available"
-								: this.props.activeMovie.movieDetails.Genre
-							} 
-						</span>
+				<main className="movie-info">
+					<h2 className="movie-title">
+						{this.props.activeMovie.movieDetails.Title}
+					</h2>
+					<span> 
+						{this.props.activeMovie.movieDetails.Year }
+						<span className="separator">|</span> 
+						{	
+							this.props.activeMovie.movieDetails.Runtime === "N/A"
+							? "No Runtime available"
+							: this.props.activeMovie.movieDetails.Runtime
+						} 
+						<span className="separator">|</span> 
+						{	
+							this.props.activeMovie.movieDetails.Genre === "N/A"
+							? "No genre available"
+							: this.props.activeMovie.movieDetails.Genre
+						} 
+					</span>
 
-						<section className="rating">
-							{ this.props.activeMovie.movieDetails.imdbRating 
-								&& this.props.activeMovie.movieDetails.imdbRating !== "N/A"
-								? <Rating rating={this.props.activeMovie.movieDetails.imdbRating} />
-								: null
-							}
-							
-							<span className="number-rating"> {
-								this.props.activeMovie.movieDetails.imdbRating === "N/A"
-								? " - "
-								: this.props.activeMovie.movieDetails.imdbRating
-							} / 10</span>
-						</section>
-
-						<h5 className="synopsis-title">Sinopse</h5>
-						<p className="movie-synopsis">
-							{this.props.activeMovie.movieDetails.Plot === "N/A" 
-								? "No plot available"
-								: this.props.activeMovie.movieDetails.Plot
-							}
-						</p>
+					<section className="rating">
+						{ this.props.activeMovie.movieDetails.imdbRating 
+							&& this.props.activeMovie.movieDetails.imdbRating !== "N/A"
+							? <Rating rating={this.props.activeMovie.movieDetails.imdbRating} />
+							: null
+						}
 						
-						<Link to="/">
-							<button className="close-button--mobile">Voltar</button>
-						</Link>
-					</main>
+						<span className="number-rating"> {
+							this.props.activeMovie.movieDetails.imdbRating === "N/A"
+							? " - "
+							: this.props.activeMovie.movieDetails.imdbRating
+						} / 10</span>
+					</section>
 
+					<h5 className="synopsis-title">Sinopse</h5>
+					<p className="movie-synopsis">
+						{this.props.activeMovie.movieDetails.Plot === "N/A" 
+							? "No plot available"
+							: this.props.activeMovie.movieDetails.Plot
+						}
+					</p>
+					
 					<Link to="/">
-						<span className="close-button">
-							{ /* close icon */ }
-							<img 
-								className="close-icon" 
-								alt="" 
-								src="../images/ic-close@3x.png" 
-								height="16" width="16" 
-							/>
-							<span className="close-text">Voltar</span>
-						</span>
+						<button className="close-button--mobile">Voltar</button>
 					</Link>
-				</div>
-				: null
-			}
+				</main>
+
+				<Link to="/">
+					<span className="close-button">
+						{ /* close icon */ }
+						<img 
+							className="close-icon" 
+							alt="" 
+							src="../images/ic-close@3x.png" 
+							height="16" width="16" 
+						/>
+						<span className="close-text">Voltar</span>
+					</span>
+				</Link>
 			</div>
-			
 		)
 	}
 }
